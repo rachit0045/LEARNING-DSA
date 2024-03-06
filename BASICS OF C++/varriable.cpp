2579ebee17f4f0d84ef83fe2e2264f293084fc28
rachit0045 <rachitsharma9022@gmail.com>
2024-03-06 19:25:22 +0530

    comm

--- BASICS OF C++/varriable.cpp
+++ BASICS OF C++/varriable.cpp
@@ -3,8 +3,9 @@ using namespace std;
 int main()
 {
     int a,b;
-    cout<<"enter the value of a";
+    cout<<"enter the value of a :";
     cin>>a;
     cout<<"enter the value of b";
     cin>>b;
+    return 0;
 }
