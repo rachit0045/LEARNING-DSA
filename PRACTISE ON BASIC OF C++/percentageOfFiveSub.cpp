4bf048e2cfa8f7d4e415f33e70f13cbf8e298771
rachit0045 <rachitsharma9022@gmail.com>
2024-03-06 18:44:32 +0530

    com

---
+++ PRACTISE ON BASIC OF C++/percentageOfFiveSub.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespce std;
+int main(){
+    int a = 100;
+    int b = 100;
+    int c = 100;
+    int d = 100;
+    int e = 100;
+    int f = (a+b+c+d+e)/5;
+    cout<<"percentage of 5 subject is : " <<f <<endl;
+}
