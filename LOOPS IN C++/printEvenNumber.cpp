be5593d18e762d4373b584f16377a66993720837
rachit0045 <rachitsharma9022@gmail.com>
2024-03-11 18:47:18 +0530

    how to print all the even numbers from 1 to 100

---
+++ LOOPS IN C++/printEvenNumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"the even numbers from 1 to 100 :"<<endl;
+    for (int i = 1; i < 100; i++)
+    {
+        if (i%2==0)
+        {
+            cout<<i<<endl;
+        }
+        
+    }
+    
+}
