966554bc8fc97f149ac9fab04b6a1ca9192cae0d
rachit0045 <rachitsharma9022@gmail.com>
2024-03-11 19:08:53 +0530

    how to print the table of 19

--- LOOPS IN C++/printTableOfNineteen.cpp
+++ LOOPS IN C++/printTableOfNineteen.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 int main()
 {
-    int n;
-    cout << "The table of :";
-    cin >> n;
+    int n = 19;
+    cout << "The table of 19 is :" << endl;
     for (int i = 1; i <= 10; i++)
     {
         cout << i * n << endl;
