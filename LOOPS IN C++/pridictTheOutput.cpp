24cc6bb165ee1e6c4ba3e2711c106f1ec59a6f29
rachit0045 <rachitsharma9022@gmail.com>
2024-03-12 17:53:42 +0530

    pridict the output

---
+++ LOOPS IN C++/pridictTheOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int j;
+    cout<<"enter the value : ";
+    cin>>j;
+    while (j <= 10)
+    {
+        cout << j << endl;
+        j = j + 1;
+    }
+    return 0;
+}
