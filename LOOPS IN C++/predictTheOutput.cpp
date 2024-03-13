de76d4207b05ed500a2255a77706c76610945c5d
rachit0045 <rachitsharma9022@gmail.com>
2024-03-13 18:58:24 +0530

    predict the output

---
+++ LOOPS IN C++/predictTheOutput.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    while ('a' < 'b')
+        cout << "malayalam is a palindrome" << endl;
+    return 0;
+}
--- LOOPS IN C++/printOddNumberUpto100.cpp
+++ LOOPS IN C++/printOddNumberUpto100.cpp
@@ -1,15 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    cout<<"odd number :"<<endl;
+    cout << "odd number :" << endl;
     for (int i = 1; i < 100; i++)
     {
-        if (i%2!=0)
+        if (i % 2 != 0)
         {
-            cout<<i <<endl;
+            cout << i << endl;
         }
-        
     }
     return 0;
 }
