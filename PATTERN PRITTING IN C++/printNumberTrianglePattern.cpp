37dcd3fe1caee6d710d0969a46e928d0a663428c
rachit0045 <rachitsharma9022@gmail.com>
2024-03-14 17:35:29 +0530

    print number triangle pattern

---
+++ PATTERN PRITTING IN C++/printNumberTrianglePattern.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int r = 5;
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout <<j;
+        }
+
+        cout << endl;
+    }
+}
