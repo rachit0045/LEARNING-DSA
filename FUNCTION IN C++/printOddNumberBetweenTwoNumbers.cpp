f230d2104aacc49d5cb17469cc8032bffc7beff8
rachit0045 <rachitsharma9022@gmail.com>
2024-03-17 21:48:41 +0530

    print odd number between two numbers

---
+++ FUNCTION IN C++/printOddNumberBetweenTwoNumbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int oddnumber(int a, int b)
+{
+    cout << "Odd number between " << a << " and " << b << " are :";
+    for (int i = a; i <= b; i++)
+    {
+        if (i % 2 != 0)
+        {
+            cout << i << ",";
+        }
+    }
+}
+int main()
+{
+    int num1, num2;
+    cout << "enter the first number :";
+    cin >> num1;
+    cout << "enter the second number :";
+    cin >> num2;
+    oddnumber(num1, num2);
+    return 0;
+}
