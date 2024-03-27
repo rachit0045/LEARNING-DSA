cf5205de1b2e078ae4b83c4048dab84902bd8bb0
rachit0045 <rachitsharma9022@gmail.com>
2024-03-27 11:54:30 +0530

    find maximum values in array

--- ARRAY IN C++/findMaximunValueInArray.cpp
+++ ARRAY IN C++/findMaximunValueInArray.cpp
@@ -5,15 +5,15 @@ int main()
     int arr[100], size;
     cout << "Enter the size of array :";
     cin >> size;
-    int max_value = arr[0];
     cout << "Enter the elements of array :";
     for (int i = 0; i < size; i++)
     {
         cin >> arr[i];
     }
+    int max_value = arr[0];
     for (int i = 0; i < size; i++)
     {
-        if (arr[i] < max_value)
+        if (arr[i] > max_value)
         {
             max_value = arr[i];
         }
