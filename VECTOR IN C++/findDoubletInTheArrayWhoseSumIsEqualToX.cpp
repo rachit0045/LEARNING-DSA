5f3dd8908deb8ffe24b4d49189b73e61b0f337f2
rachit0045 <rachitsharma9022@gmail.com>
2024-03-29 15:44:35 +0530

    Copy the contents of the original array into the reversed array in reverse order

--- VECTOR IN C++/findDoubletInTheArrayWhoseSumIsEqualToX.cpp
+++ VECTOR IN C++/findDoubletInTheArrayWhoseSumIsEqualToX.cpp
@@ -5,7 +5,7 @@ int main()
     int arr[100], size, x;
     cout << "Enter the size of array :";
     cin >> size;
-    cout<<"Enter the elements of array :";
+    cout << "Enter the elements of array :";
     for (int i = 0; i < size; i++)
     {
         cin >> arr[i];
@@ -23,6 +23,7 @@ int main()
             }
         }
     }
-    cout << " Doublet in the Array whose sum is equal to " << x <<" are : "<< doublet << endl;
+    cout << " Doublet in the Array whose sum is equal to " << x << " are : " << doublet << endl;
+
     return 0;
 }
