6b13c62275fac00edc5acd3c9f7b384eaf3ad9ce
rachit0045 <rachitsharma9022@gmail.com>
2024-03-30 13:36:23 +0530

    store roll number and marks

---
+++ ARRAY IN C++/2D ARRAY/storeTheRollNoAndMarks.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row, col;
+    cout << "Enter the rows :";
+    cin >> row;
+    cout << "Enter the columns :";
+    cin >> col;
+    int matrix[row][col];
+    for (int i = 0; i < row; i++)
+    {
+        cout << "Enter the roll number for student " << i + 1 << " : ";
+        cin >> matrix[i][0];
+        cout << "Enter the marks for student " << i + 1 << " : ";
+        cin >> matrix[i][1];
+        cout << endl;
+    }
+    cout << "Roll Number\tmarks "<<endl;
+    cout << "-----------------------"<<endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << matrix[i][j]<<"\t\t";
+        }
+        cout << endl;
+    }
+    return 0;
+}
