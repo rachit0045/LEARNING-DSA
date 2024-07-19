dbb32acc3156a099644051701e0790349235d663
rachit0045 <rachitsharma9022@gmail.com>
2024-07-19 09:46:22 +0530

    code to access and print the third element

---
+++ ARRAY 2.0/FUNDAMENTAL OF ARRAY/WORKSHEET-1/question6.cpp
@@ -0,0 +1,16 @@
+//  Given an array int arr[5] = {10, 20, 30, 40, 50};, write the code to access and print the third element.//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // declare and initialization of array//
+
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    // code to access and print the third element//
+
+    cout << "The third element of array is : " << arr[2];
+
+    return 0;
+}
