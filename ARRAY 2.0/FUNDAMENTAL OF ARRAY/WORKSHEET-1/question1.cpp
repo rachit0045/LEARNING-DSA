3cd4651e8aa5bf7cdff8253da011ffb801f051cb
rachit0045 <rachitsharma9022@gmail.com>
2024-07-19 09:28:15 +0530

    Explain array

---
+++ ARRAY 2.0/FUNDAMENTAL OF ARRAY/WORKSHEET-1/question1.cpp
@@ -0,0 +1,3 @@
+// Explainwhat an array is in C++.//
+
+// ARRAY : Array is a linear data structure where all elements are arranged sequentially. It is collection of same data type store at  contiguous memory location.//
