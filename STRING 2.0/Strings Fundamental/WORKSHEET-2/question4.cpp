1e28881be9c95ce7cd1c0bc4f43efcd7fadcf049
rachit0045 <rachitsharma9022@gmail.com>
2024-08-09 15:30:39 +0530

    Program to find length of string

--- STRING 2.0/Strings Fundamental/WORKSHEET-2/question4.cpp
+++ STRING 2.0/Strings Fundamental/WORKSHEET-2/question4.cpp
@@ -9,7 +9,6 @@ int main()
     string s;
     cout << "Enter the string : ";
     getline(cin, s);
-    // int n = s.length();
     cout << "Length of string is " << s.length() << endl;
     return 0;
 }
